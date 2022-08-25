c1650884e0ffdb0bad047bdcc920ff5824d1fc36
mj0107 <alswns1285@gmail.com>
2022-08-25 20:04:06 +0900

    연속되는 수 (수정)

--- 54.js
+++ 54.js
@@ -1,11 +1,18 @@
 function isConsecutiveNumber(str) {
-  const str2 = str
-    .split(" ")
-    .sort((a, b) => a - b)
-    .join(" ");
+  let arr = str.split(" ").map(Number);
 
-  console.log(str === str2 ? "YES" : "NO");
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[0] + i) return "NO";
+  }
+
+  return "YES";
 }
 
-const str = "1 2 3 4 5 2";
-isConsecutiveNumber(str);
+const str = "1 2 3 4 5"; // YES
+/*
+  1 4 2 6 3 // NO
+  1 2 3 4   // 공백을 입력받았을 경우 NO
+  1 2 3 4 hello // 숫자가 아닌 문자가 들어갔을 경우 NO
+*/
+const result = isConsecutiveNumber(str);
+console.log(result);
