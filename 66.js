8ca4d70da7f33a24524db0836dfbdb373bae7989
mj0107 <alswns1285@gmail.com>
2022-09-05 21:17:58 +0900

    블럭탑쌓기

---
+++ 66.js
@@ -0,0 +1,20 @@
+function blockTowerStack(towers, rule) {
+  let result = [];
+
+  for (const tower of towers) {
+    let rule_idx = [];
+
+    rule.split("").forEach((v) => rule_idx.push(tower.indexOf(v)));
+    rule_idx = rule_idx.filter((v) => v !== -1);
+
+    if (rule_idx.join() === rule_idx.sort().join()) result.push("가능");
+    else result.push("불가능");
+  }
+
+  console.log(result);
+}
+
+const towers = ["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"];
+const rule = "ABD";
+
+blockTowerStack(towers, rule);
