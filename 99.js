89940a2b8589b7b9b7d79620d84b50dcb05dab6c
mj0107 <alswns1285@gmail.com>
2022-10-14 21:02:52 +0900

    토끼들의 행진

---
+++ 99.js
@@ -0,0 +1,24 @@
+function solution() {
+  let stoneDurabilityArr = [1, 2, 1, 4, 5, 2];
+  const RABBIT_JUMP_POWER_ARR = [2, 1, 3, 1];
+
+  const BRIDGE_LENGTH = stoneDurabilityArr.length;
+
+  let result = [];
+
+  let isPossiblePassed = true;
+  for (const POWER of RABBIT_JUMP_POWER_ARR) {
+    isPossiblePassed = true;
+    for (let i = POWER - 1; i < BRIDGE_LENGTH; i += POWER) {
+      stoneDurabilityArr[i] -= 1;
+      if (stoneDurabilityArr[i] < 0) isPossiblePassed = false;
+    }
+
+    if (isPossiblePassed) result.push('pass');
+    else result.push('fail');
+  }
+
+  console.log(result);
+}
+
+solution();
